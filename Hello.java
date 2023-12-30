fe8f1830dad66f3a9a639b61a36931d457447ae8
mitraanu <107745773+mitraanu@users.noreply.github.com>
2023-12-30 10:01:20 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello to Java");
+		System.out.println("Hello to Git");		
+
+	}
+
+}
