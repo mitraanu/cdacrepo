5875ec521a59b8414111f4fbb48df737b8adf546
mitraanu <107745773+mitraanu@users.noreply.github.com>
2025-07-28 11:55:15 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is just a hello message");
+		System.out.println("I am printing through Github actions");
+
+	}
+
+}
